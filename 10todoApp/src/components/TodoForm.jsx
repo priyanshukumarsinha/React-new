34b153b6788099d7f778ce70090d8093fcecd340
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-26 01:46:05 +0000

    Project 10 : Components Ready

--- 10todoApp/src/components/TodoForm.jsx
+++ 10todoApp/src/components/TodoForm.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTodo } from '../context'
 
 function TodoForm() {
+    const [todo, setTodo] = useState("")    
+    const {addTodo} = useTodo()
 
+    const add = () => {
+        addTodo({id : Date.now(), todo : todo, completed : false})
+    }
 
     return (
-        <form className="flex">
+        <form onSubmit= {add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
+                onChange={() => setTodo(todo)}
+                value={todo}
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">

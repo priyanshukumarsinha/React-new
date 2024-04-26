218a6df322f152c2a09025a67da21f00b54a0c81
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-26 00:35:04 +0000

    Project 10 : Context API Setup Done

---
+++ 10todoApp/src/App.jsx
@@ -0,0 +1,14 @@
+import {TodoProvider} from './context'
+
+
+function App() {
+  return (
+    <TodoProvider>
+
+      <h1 className="w-full text-center text-3xl">Manage Todo App</h1>
+
+    </TodoProvider>
+  )
+}
+
+export default App
---
+++ 10todoApp/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { createContext, useContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todo : "Todo Msg",
+            completed : false
+        },
+    ],
+    addTodo : (todo) => {},
+    updateTodo : (id, todo) => {},
+    removeTodo : (id) => {},
+    toggleCompleted : (id) => {},
+});
+
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}

b3ea3b13d7f9b3009694b63971a7479be05e05dc
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-27 15:02:27 +0000

    Project 12 : Create Select Component

---
+++ 12megaBlog/src/components/Select.jsx
@@ -0,0 +1,30 @@
+import React, { useId } from 'react'
+
+function Select(
+    {options,
+    label,
+    className = "",
+    ...props}, ref
+) {
+
+    const id = useId();
+  return (
+    <div>
+        { label && <label htmlFor= {id} className='' ></label>}
+        <select
+            className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            id = {id}
+            {...props}
+        >
+            {options?.map((option) =>{
+                <option value={option} key = {option} >
+                    {option}
+                </option>
+            })}
+        </select>
+    </div>
+  )
+}
+
+export default React.forwardRef(Select)
